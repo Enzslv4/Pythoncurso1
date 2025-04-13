95559ecb84bea327ddb25bdc70e8bd1573af56e2
Enzo Silva <enzosilva142002@gmail.com>
2025-04-13 11:52:41 -0300

    terminando decoradores

--- aula103.py
+++ aula103.py
@@ -4,9 +4,21 @@
 # Decoradores são usados para fazer o Python
 # usar as funções decoradoras em outras funções.
 
-def inverte_string(string):
+def createFunction(func):
+    def intern(*args, **kwargs):
+        for arg in args:
+            isString(arg)
+            result = func(*args, **kwargs)
+            return result
+    return intern
+
+def invertString(string):
     return string[::-1]
 
-invertida = inverte_string('Enzo')
+def isString(param):
+    if not isinstance(param, str):
+        raise TypeError('Param deve ser uma string. ')
 
-print(invertida)
+invertStringCheckParam = createFunction(invertString)
+inverted = invertStringCheckParam('Enzo')
+print(inverted)
